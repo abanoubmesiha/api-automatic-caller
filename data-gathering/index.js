9b4ccdee9b46cc460a48a00dcf6f7b564152e6ae
abanoubmesiha <anagy@app-corp.com>
2022-04-06 13:37:18 +0200

    Create a `dataGathering` function.

---
+++ data-gathering/index.js
@@ -0,0 +1,32 @@
+import fetch from 'node-fetch';
+import {
+    writeToJsonFile,
+    readTimeSchedule,
+    isAutomaticAPICallingActive,
+    isTimeIsOver,
+    readMinutesInterval
+} from '../utils/index.js';
+
+const {API_LINK, APP_KEY} = process.env;
+
+const timeSchedule = readTimeSchedule();
+const minutesInterval = readMinutesInterval();
+
+export const dataGathering = () => {
+    const checkScheduleInterval = setInterval(
+        async () => {
+            if (isAutomaticAPICallingActive(timeSchedule)) {
+                const data = await fetch(API_LINK, {headers: {'application_key': APP_KEY}})
+                    .then(response => response.json())
+                    .then(response => JSON.stringify(response));
+                console.log("Make an API call...");
+                writeToJsonFile(data);
+            }
+            if (isTimeIsOver(timeSchedule)) {
+                console.log("Stop API calling...");
+                clearInterval(checkScheduleInterval)
+            }
+        },
+        1000 * 60 * minutesInterval
+    );
+};
--- index.js
+++ index.js
@@ -1,42 +1,12 @@
 import express from 'express';
-import fetch from 'node-fetch';
 import 'dotenv/config';
-import {
-  writeToJsonFile,
-  readTimeSchedule,
-  isAutomaticAPICallingActive,
-  isTimeIsOver,
-  readMinutesInterval
-} from './utils/index.js';
+import {dataGathering} from './data-gathering/index.js';
 
 const app = express();
 const port = 3456;
-const {API_LINK, APP_KEY} = process.env;
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-const timeSchedule = readTimeSchedule();
-const minutesInterval = readMinutesInterval();
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`)
 
-  const checkScheduleInterval = setInterval(
-    async () => {
-      if (isAutomaticAPICallingActive(timeSchedule)) {
-        const data = await fetch(API_LINK, {headers: {'application_key': API_LINK}})
-          .then(response => response.json())
-          .then(response => JSON.stringify(response));
-        console.log("Make an API call...");
-        writeToJsonFile(data);
-      }
-      if (isTimeIsOver(timeSchedule)) {
-        console.log("Stop API calling...");
-        clearInterval(checkScheduleInterval)
-      }
-    },
-    1000 * 60 * minutesInterval
-  );
+  dataGathering();
 });
